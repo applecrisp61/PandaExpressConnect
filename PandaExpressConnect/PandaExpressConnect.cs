342814205088ee498f208b529f1f3682f40fecc2
Daniel Letzler <daniel.letzler@gmail.com>
2017-02-08 17:37:02 -0800

    Initial creation
    Trying to get something that is so darn simple I can figure out why the
    iPhone is no longer connecting to Azure Services

---
+++ PandaExpressConnect/PandaExpressConnect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace PandaExpressConnect {
+	public class App : Application {
+		public App() {
+
+			var testingPage = new TestingPage();
+
+			MainPage = new NavigationPage(testingPage);
+		}
+
+		protected override void OnStart() {
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep() {
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume() {
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ PandaExpressConnect/TestingPage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+using Xamarin.Forms;
+
+using Microsoft.WindowsAzure.MobileServices;
+using FlirtyLocation;
+
+namespace PandaExpressConnect {
+	public class TestingPage : ContentPage {
+		public TestingPage() {
+			Content = new StackLayout {
+
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center,
+
+				Children = {
+					new Label { Text = "Testing Page" }
+				}
+			};
+
+			Device.StartTimer(TimeSpan.FromSeconds(5), () => {
+				var msg = TestPandaConnect();
+
+				var sl = this.Content as StackLayout;
+				sl?.Children.Add(new Label { Text = msg });
+
+				return true;
+			});
+		}
+
+		private string TestPandaConnect() {
+			LocationMeasurementManager commMgr = null;
+			StringBuilder sb = new StringBuilder();
+
+			try {
+				commMgr = LocationMeasurementManager.DefaultManager;
+			}
+			catch (MobileServiceInvalidOperationException msioe) {
+				sb.AppendLine($"Mobile service invalid operation exception: {msioe.Message}");
+				return sb.ToString();
+			}
+			catch (Exception e) {
+				sb.AppendLine($"Exception: {e.Message}");
+				sb.AppendLine($"Inner exception: {e.InnerException}");
+				return sb.ToString();
+			}
+
+			return $"CommMgr established without exception. {commMgr.CurrentClient.MobileAppUri.AbsolutePath}";
+
+		}
+	}
+}
+
